c68dc249d01f4c08b74323ab0a211127500a2005
jnzhang622 <jnzhang622@gmail.com>
2020-11-01 00:29:49 -0400

    updated fetch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ class App extends React.Component {
   state = {
     rawData: [],
     playlist: [],
+    totalResults: null,
+
     code: "PLuF8GDJO9aZLEdVIiCJQgkEeHSocIEy5r",
   }
 
@@ -11,15 +13,20 @@ class App extends React.Component {
     this.fetchPlaylist()
   }
 
-  fetchPlaylist = (e) => {
+  fetchPlaylist = () => {
     fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${this.state.code}&key=${process.env.REACT_APP_API_KEY}`)
       .then(resp => resp.json())
-      .then(data => this.setState({ rawData: data }))
+      .then(data => this.setState({ 
+        rawData: data, 
+        totalResults: data.pageInfo.totalResults,
+        playlist: [...this.state.playlist, data.items]
+      }))
   }
 
 
   render() {
-    console.log(this.state.rawData)
+    // console.log(this.state.totalResults)
+    console.log(this.state.playlist)
     return(
     <div>
       
